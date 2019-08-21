ccf8480523aac541f7684554b379c73122b59d9e
zscome <zscomehuyue@126.com>
2019-08-21 14:25:10 +0800

    add read appname for spring.boot application name

--- cat-client/src/main/java/com/dianping/cat/configuration/DefaultClientConfigManager.java
+++ cat-client/src/main/java/com/dianping/cat/configuration/DefaultClientConfigManager.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import com.dianping.cat.CatPropertyProvider;
 import com.site.helper.JsonBuilder;
 import com.site.helper.Splitters;
 import org.codehaus.plexus.logging.LogEnabled;
@@ -47,287 +48,287 @@ import com.dianping.cat.message.spi.MessageTree;
 @Named(type = ClientConfigManager.class)
 public class DefaultClientConfigManager implements LogEnabled, ClientConfigManager, Initializable {
 
-	private static final String PROPERTIES_FILE = "/META-INF/app.properties";
+    private static final String PROPERTIES_FILE = "/META-INF/app.properties";
 
-	private ClientConfig m_config;
+    private ClientConfig m_config;
 
-	private volatile double m_sampleRate = 1d;
+    private volatile double m_sampleRate = 1d;
 
-	private volatile boolean m_block = false;
+    private volatile boolean m_block = false;
 
-	private String m_routers;
+    private String m_routers;
 
-	private JsonBuilder m_jsonBuilder = new JsonBuilder();
+    private JsonBuilder m_jsonBuilder = new JsonBuilder();
 
-	private AtomicTreeParser m_atomicTreeParser = new AtomicTreeParser();
+    private AtomicTreeParser m_atomicTreeParser = new AtomicTreeParser();
 
-	private Map<String, List<Integer>> m_longConfigs = new LinkedHashMap<String, List<Integer>>();
+    private Map<String, List<Integer>> m_longConfigs = new LinkedHashMap<String, List<Integer>>();
 
-	private Logger m_logger;
+    private Logger m_logger;
 
-	@Override
-	public void enableLogging(Logger logger) {
-		m_logger = logger;
-	}
+    @Override
+    public void enableLogging(Logger logger) {
+        m_logger = logger;
+    }
 
-	@Override
-	public Domain getDomain() {
-		Domain domain = null;
+    @Override
+    public Domain getDomain() {
+        Domain domain = null;
 
-		if (m_config != null) {
-			Map<String, Domain> domains = m_config.getDomains();
+        if (m_config != null) {
+            Map<String, Domain> domains = m_config.getDomains();
 
-			domain = domains.isEmpty() ? null : domains.values().iterator().next();
-		}
+            domain = domains.isEmpty() ? null : domains.values().iterator().next();
+        }
 
-		if (domain != null) {
-			return domain;
-		} else {
-			return new Domain("UNKNOWN").setEnabled(false);
-		}
-	}
+        if (domain != null) {
+            return domain;
+        } else {
+            return new Domain("UNKNOWN").setEnabled(false);
+        }
+    }
 
-	@Override
-	public int getMaxMessageLength() {
-		if (m_config == null) {
-			return 5000;
-		} else {
-			return getDomain().getMaxMessageSize();
-		}
-	}
+    @Override
+    public int getMaxMessageLength() {
+        if (m_config == null) {
+            return 5000;
+        } else {
+            return getDomain().getMaxMessageSize();
+        }
+    }
 
-	@Override
-	public String getRouters() {
-		if (m_routers == null) {
-			refreshConfig();
-		}
-		return m_routers;
-	}
+    @Override
+    public String getRouters() {
+        if (m_routers == null) {
+            refreshConfig();
+        }
+        return m_routers;
+    }
 
-	public double getSampleRatio() {
-		return m_sampleRate;
-	}
+    public double getSampleRatio() {
+        return m_sampleRate;
+    }
 
-	private String getServerConfigUrl() {
-		if (m_config == null) {
-			return null;
-		} else {
-			List<Server> servers = m_config.getServers();
-			int size = servers.size();
-			int index = (int) (size * Math.random());
+    private String getServerConfigUrl() {
+        if (m_config == null) {
+            return null;
+        } else {
+            List<Server> servers = m_config.getServers();
+            int size = servers.size();
+            int index = (int) (size * Math.random());
 
-			if (index >= 0 && index < size) {
-				Server server = servers.get(index);
+            if (index >= 0 && index < size) {
+                Server server = servers.get(index);
 
-				Integer httpPort = server.getHttpPort();
+                Integer httpPort = server.getHttpPort();
 
-				if (httpPort == null || httpPort == 0) {
-					httpPort = 8080;
-				}
-				return String.format("http://%s:%d/cat/s/router?domain=%s&ip=%s&op=json", server.getIp().trim(), httpPort,
-										getDomain().getId(), NetworkInterfaceManager.INSTANCE.getLocalHostAddress());
-			}
-		}
-		return null;
-	}
+                if (httpPort == null || httpPort == 0) {
+                    httpPort = 8080;
+                }
+                return String.format("http://%s:%d/cat/s/router?domain=%s&ip=%s&op=json", server.getIp().trim(), httpPort,
+                        getDomain().getId(), NetworkInterfaceManager.INSTANCE.getLocalHostAddress());
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public List<Server> getServers() {
-		if (m_config == null) {
-			return Collections.emptyList();
-		} else {
-			return m_config.getServers();
-		}
-	}
+    @Override
+    public List<Server> getServers() {
+        if (m_config == null) {
+            return Collections.emptyList();
+        } else {
+            return m_config.getServers();
+        }
+    }
 
-	@Override
-	public int getTaggedTransactionCacheSize() {
-		return 1024;
-	}
+    @Override
+    public int getTaggedTransactionCacheSize() {
+        return 1024;
+    }
 
-	@Override
-	public void initialize() throws InitializationException {
-		String xml = Cat.getCatHome() + "client.xml";
-		File configFile = new File(xml);
+    @Override
+    public void initialize() throws InitializationException {
+        String xml = Cat.getCatHome() + "client.xml";
+        File configFile = new File(xml);
 
-		m_logger.info("client xml path " + xml);
-		initialize(configFile);
-	}
+        m_logger.info("client xml path " + xml);
+        initialize(configFile);
+    }
 
-	@Override
-	public void initialize(File configFile) throws InitializationException {
-		try {
-			ClientConfig globalConfig = null;
-			ClientConfig warConfig = null;
+    @Override
+    public void initialize(File configFile) throws InitializationException {
+        try {
+            ClientConfig globalConfig = null;
+            ClientConfig warConfig = null;
+            if (configFile != null) {
+                if (configFile.exists()) {
+                    String xml = Files.forIO().readFrom(configFile.getCanonicalFile(), "utf-8");
 
-			if (configFile != null) {
-				if (configFile.exists()) {
-					String xml = Files.forIO().readFrom(configFile.getCanonicalFile(), "utf-8");
+                    globalConfig = DefaultSaxParser.parse(xml);
+                    m_logger.info(String.format("Global config file(%s) found.", configFile));
+                } else {
+                    m_logger.warn(String.format("Global config file(%s) not found, IGNORED.", configFile));
+                }
+            }
 
-					globalConfig = DefaultSaxParser.parse(xml);
-					m_logger.info(String.format("Global config file(%s) found.", configFile));
-				} else {
-					m_logger.warn(String.format("Global config file(%s) not found, IGNORED.", configFile));
-				}
-			}
+            // load the client configure from Java class-path
+            warConfig = loadConfigFromEnviroment();
 
-			// load the client configure from Java class-path
-			warConfig = loadConfigFromEnviroment();
+            // merge the two configures together to make it effected
+            if (globalConfig != null && warConfig != null) {
+                globalConfig.accept(new ClientConfigMerger(warConfig));
+            }
 
-			// merge the two configures together to make it effected
-			if (globalConfig != null && warConfig != null) {
-				globalConfig.accept(new ClientConfigMerger(warConfig));
-			}
+            if (warConfig != null) {
+                warConfig.accept(new ClientConfigValidator());
+            }
 
-			if (warConfig != null) {
-				warConfig.accept(new ClientConfigValidator());
-			}
+            m_config = warConfig;
+            refreshConfig();
+        } catch (Exception e) {
+            throw new InitializationException(e.getMessage(), e);
+        }
+    }
 
-			m_config = warConfig;
-			refreshConfig();
-		} catch (Exception e) {
-			throw new InitializationException(e.getMessage(), e);
-		}
-	}
+    @Override
+    public boolean isAtomicMessage(MessageTree tree) {
+        return m_atomicTreeParser.isAtomicMessage(tree);
+    }
 
-	@Override
-	public boolean isAtomicMessage(MessageTree tree) {
-		return m_atomicTreeParser.isAtomicMessage(tree);
-	}
+    public boolean isBlock() {
+        return m_block;
+    }
 
-	public boolean isBlock() {
-		return m_block;
-	}
+    @Override
+    public boolean isCatEnabled() {
+        if (m_config == null) {
+            return false;
+        } else {
+            return m_config.isEnabled();
+        }
+    }
 
-	@Override
-	public boolean isCatEnabled() {
-		if (m_config == null) {
-			return false;
-		} else {
-			return m_config.isEnabled();
-		}
-	}
+    @Override
+    public boolean isDumpLocked() {
+        if (m_config == null) {
+            return false;
+        } else {
+            return m_config.isDumpLocked();
+        }
+    }
 
-	@Override
-	public boolean isDumpLocked() {
-		if (m_config == null) {
-			return false;
-		} else {
-			return m_config.isDumpLocked();
-		}
-	}
+    private ClientConfig loadConfigFromEnviroment() {
+        String appName = loadProjectName();
+        if (null == appName || "".equals(appName)) {
+            appName = CatPropertyProvider.INST.getProperty("spring.application.name", "appName");
+        }
+        if (appName != null) {
+            ClientConfig config = new ClientConfig();
+            config.addDomain(new Domain(appName));
+            return config;
+        }
+        return null;
+    }
 
-	private ClientConfig loadConfigFromEnviroment() {
-		String appName = loadProjectName();
+    private String loadProjectName() {
+        String appName = null;
+        InputStream in = null;
+        try {
+            in = Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPERTIES_FILE);
 
-		if (appName != null) {
-			ClientConfig config = new ClientConfig();
+            if (in == null) {
+                in = Cat.class.getResourceAsStream(PROPERTIES_FILE);
+            }
+            if (in != null) {
+                Properties prop = new Properties();
 
-			config.addDomain(new Domain(appName));
-			return config;
-		}
-		return null;
-	}
+                prop.load(in);
 
-	private String loadProjectName() {
-		String appName = null;
-		InputStream in = null;
-		try {
-			in = Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPERTIES_FILE);
+                appName = prop.getProperty("app.name");
+                if (appName != null) {
+                    m_logger.info(String.format("Find domain name %s from app.properties.", appName));
+                } else {
+                    m_logger.info(String.format("Can't find app.name from app.properties."));
+                    return null;
+                }
+            } else {
+                m_logger.info(String.format("Can't find app.properties in %s", PROPERTIES_FILE));
+            }
+        } catch (Exception e) {
+            m_logger.error(e.getMessage(), e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+        return appName;
+    }
 
-			if (in == null) {
-				in = Cat.class.getResourceAsStream(PROPERTIES_FILE);
-			}
-			if (in != null) {
-				Properties prop = new Properties();
+    public void refreshConfig() {
+        String url = getServerConfigUrl();
 
-				prop.load(in);
+        try {
+            InputStream inputstream = Urls.forIO().readTimeout(2000).connectTimeout(1000).openStream(url);
+            String content = Files.forIO().readFrom(inputstream, "utf-8");
+            KVConfig routerConfig = (KVConfig) m_jsonBuilder.parse(content.trim(), KVConfig.class);
 
-				appName = prop.getProperty("app.name");
-				if (appName != null) {
-					m_logger.info(String.format("Find domain name %s from app.properties.", appName));
-				} else {
-					m_logger.info(String.format("Can't find app.name from app.properties."));
-					return null;
-				}
-			} else {
-				m_logger.info(String.format("Can't find app.properties in %s", PROPERTIES_FILE));
-			}
-		} catch (Exception e) {
-			m_logger.error(e.getMessage(), e);
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (Exception e) {
-				}
-			}
-		}
-		return appName;
-	}
+            m_routers = routerConfig.getValue("routers");
+            m_block = Boolean.valueOf(routerConfig.getValue("block").trim());
 
-	public void refreshConfig() {
-		String url = getServerConfigUrl();
+            m_sampleRate = Double.valueOf(routerConfig.getValue("sample").trim());
+            if (m_sampleRate <= 0) {
+                m_sampleRate = 0;
+            }
 
-		try {
-			InputStream inputstream = Urls.forIO().readTimeout(2000).connectTimeout(1000).openStream(url);
-			String content = Files.forIO().readFrom(inputstream, "utf-8");
-			KVConfig routerConfig = (KVConfig) m_jsonBuilder.parse(content.trim(), KVConfig.class);
+            String startTypes = routerConfig.getValue("startTransactionTypes");
+            String matchTypes = routerConfig.getValue("matchTransactionTypes");
 
-			m_routers = routerConfig.getValue("routers");
-			m_block = Boolean.valueOf(routerConfig.getValue("block").trim());
+            m_atomicTreeParser.init(startTypes, matchTypes);
 
-			m_sampleRate = Double.valueOf(routerConfig.getValue("sample").trim());
-			if (m_sampleRate <= 0) {
-				m_sampleRate = 0;
-			}
+            for (ProblemLongType longType : ProblemLongType.values()) {
+                final String name = longType.getName();
+                String propertyName = name + "s";
+                String values = routerConfig.getValue(propertyName);
 
-			String startTypes = routerConfig.getValue("startTransactionTypes");
-			String matchTypes = routerConfig.getValue("matchTransactionTypes");
+                if (values != null) {
+                    List<String> valueStrs = Splitters.by(',').trim().split(values);
+                    List<Integer> thresholds = new LinkedList<Integer>();
 
-			m_atomicTreeParser.init(startTypes, matchTypes);
+                    for (String valueStr : valueStrs) {
+                        try {
+                            thresholds.add(Integer.parseInt(valueStr));
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                    }
+                    if (!thresholds.isEmpty()) {
+                        m_longConfigs.put(name, thresholds);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            m_logger.warn("error when connect cat server config url " + url);
+        }
+    }
 
-			for (ProblemLongType longType : ProblemLongType.values()) {
-				final String name = longType.getName();
-				String propertyName = name + "s";
-				String values = routerConfig.getValue(propertyName);
+    @Override
+    public int getLongThresholdByDuration(String key, int duration) {
+        List<Integer> values = m_longConfigs.get(key);
 
-				if (values != null) {
-					List<String> valueStrs = Splitters.by(',').trim().split(values);
-					List<Integer> thresholds = new LinkedList<Integer>();
+        if (values != null) {
+            for (int i = values.size() - 1; i >= 0; i--) {
+                int userThreshold = values.get(i);
 
-					for (String valueStr : valueStrs) {
-						try {
-							thresholds.add(Integer.parseInt(valueStr));
-						} catch (Exception e) {
-							// ignore
-						}
-					}
-					if (!thresholds.isEmpty()) {
-						m_longConfigs.put(name, thresholds);
-					}
-				}
-			}
-		} catch (Exception e) {
-			m_logger.warn("error when connect cat server config url " + url);
-		}
-	}
+                if (duration >= userThreshold) {
+                    return userThreshold;
+                }
+            }
+        }
 
-	@Override
-	public int getLongThresholdByDuration(String key, int duration) {
-		List<Integer> values = m_longConfigs.get(key);
-
-		if (values != null) {
-			for (int i = values.size() - 1; i >= 0; i--) {
-				int userThreshold = values.get(i);
-
-				if (duration >= userThreshold) {
-					return userThreshold;
-				}
-			}
-		}
-
-		return -1;
-	}
+        return -1;
+    }
 
 }
